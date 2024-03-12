60126f219ed18b621dafd8718c9a9f30e894ef19
Skyler-Dominik-England <150696786+ItsSpo0ks@users.noreply.github.com>
2024-03-12 00:51:42 -0400

    ProjetSonic-I Partie 2 Animation gauche droite début

--- Assets/Scripts/ControlerPersonnage.cs
+++ Assets/Scripts/ControlerPersonnage.cs
@@ -2,42 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/* Gestion de déplacement et du saut du personnage à l'aide des touches : a, d et w      
-* Gestion des détections de collision entre le personnage et les objets du jeu  
-* Par: Vahik Toroussian
-* Modifié: 5/12/2018
-*/
+// Script de contrôle des déplacements, des sauts de Sonic et des detections de collisions.
+// Par : Skyler-Dominik England
+// Dernière modification : 10/03/2024
 public class ControlerPersonnage : MonoBehaviour
 {
+    // |||||||||||||||||||||||||||||||||||||||| Déclaration des Variables |||||||||||||||||||||||||||||||||||||||| \\
     public float vitesseX;      //vitesse horizontale actuelle
     public float vitesseXMax;   //vitesse horizontale Maximale désirée
     public float vitesseY;      //vitesse verticale 
     public float vitesseSaut;   //vitesse de saut désirée
 
+    // ----------------------------------------------------------------------------------------------------------- \\
     /* Détection des touches et modification de la vitesse de déplacement;
        "a" et "d" pour avancer et reculer, "w" pour sauter
     */
     void Update ()
     {
-        // déplacement vers la gauche
+        // Déplacement vers la gauche.
         if (Input.GetKey("a"))
         {
             vitesseX = -vitesseXMax;
+            GetComponent<SpriteRenderer>().flipX = true;
 
         }
-        else if (Input.GetKey("d"))   //déplacement vers la droite
+        // Déplacement vers la droite.
+        else if (Input.GetKey("d"))
         {
             vitesseX = vitesseXMax;
+            GetComponent<SpriteRenderer>().flipX = false;
         }
         else
         {
             vitesseX = GetComponent<Rigidbody2D>().velocity.x;  //mémorise vitesse actuelle en X
         }
 
-        // sauter l'objet à l'aide la touche "w"
+        // Sauter l'objet à l'aide la touche "w".
         if (Input.GetKeyDown("w"))
         {
             vitesseY = vitesseSaut;
+
         }
         else
         {
@@ -51,9 +55,18 @@ public class ControlerPersonnage : MonoBehaviour
         //**************************Gestion des animaitons de course et de repos********************************
         //Active l'animation de course si la vitesse de déplacement n'est pas 0, sinon le repos sera jouer par Animator
 
-      
-       
-       
+        if(vitesseX > 0.1f || vitesseX < -0.1f) 
+        { 
+            GetComponent<Animator>().SetBool("marche", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("marche", false);
+        }
+
+
+
+
 
 
     }
